9e92055f10dcc68115285e4adcb1120aa676655d
Robby Rice <ricerob@iastate.edu>
2022-04-26 14:37:40 -0500

    webpack issues :(

--- src/SendPage.js
+++ src/SendPage.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Web3 from "web3";
 
 const SendPage = () => {
     let params = useParams();
+    let navigate = useNavigate();
+    if(!Web3.utils.isAddress(params.addr)){
+        navigate('/');
+    }
     return <h3>Send ETH to: {params.addr}</h3>
 }
 
